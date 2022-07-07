35f4168ae1c0a19cbd8e676c4ca4383ceb67ba6e
Roman <roman@MacBook-Pro-Roman.local>
2022-07-07 18:29:08 +0200

    add new files

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
-            <Header/>
-            <MarketingApp />
+          <Header />
+          <MarketingApp />
         </div>
-      </BrowserRouter>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
